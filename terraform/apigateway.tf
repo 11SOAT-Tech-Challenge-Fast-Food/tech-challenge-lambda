d9f14cc4dd4212c2f315dc5003e38be5a550964b
Wesley <12wesleyp@gmail.com>
2025-10-05 23:52:51 -0300

    fix deploy.yaml

--- terraform/apigateway.tf
+++ terraform/apigateway.tf
@@ -1,5 +1,5 @@
 resource "aws_api_gateway_rest_api" "main" {
-  name        = "${var.name_prefix}-api"
+  name        = "tech-challenge-api-gateway"
   description = "API for Auth + Register + EKS proxy"
 }
 
